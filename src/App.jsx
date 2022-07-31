a1b63aa5148269e0f70eb21c81b01104b3e6085b
Taylor <tayforbes92@hotmail.com>
2022-07-31 16:16:06 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState, setState } from "react";
+import DisplayIdeas from "./DisplayIdeas";
+import IdeasForm from "./IdeasForm";
+
+function App() {
+  const [ideas, setIdeas] = useState([
+    "Movie about a guy who gets bit by a radioactive mole rat",
+    "Romcom about two developers over Zoom",
+    "Horror film where AWS goes down for 24 hours"
+  ]);
+
+  const addIdeas = (newIdeas) => {
+    setIdeas([...ideas, newIdeas])
+  }
+
+  return (
+    <main>
+      <h1>Welcome, Super Awesome Movie Monster Think Tank!</h1>
+
+      <br />
+
+      <h1>Submit your ideas down below:</h1>
+      <DisplayIdeas ideas={ideas} />
+      <IdeasForm addIdeas={addIdeas} />
+    </main>
+  );
+}
+
+export default App;
+
---
+++ src/DisplayIdeas.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react"
+
+function DisplayIdeas(props) {
+  let crazyIdeas = props.ideas.map(idea => <li> {idea} </li>)
+  return (
+    <div>
+      <ul>
+        {crazyIdeas}
+      </ul>
+    </div>
+  )
+}
+
+export default DisplayIdeas
